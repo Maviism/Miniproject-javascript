1743eec94fce0ca0c934c4ead898776098861e71
Muadz Izh <muadzihharul@gmail.com>
2024-08-25 14:26:33 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,33 @@
+const kodams = [
+    'Mie Ayam',
+    'Bala-Bala',
+    'Gorengan',
+    'Cimol',
+    'Cireng',
+    'Cilok',
+    'Batagor',
+    'Seblak',
+    'Pempek',
+    'Bakso',
+    'Sate',
+    'Gado-Gado',
+    'Rendang',
+    'Nasi Uduk',
+    'Bubur Ayam',
+    'Martabak'
+];
+
+const adjectives = [
+    'Gokil',
+    'Ngakak',
+    'Kocak',
+    'Absurd',
+    'Lucu',
+    'Konyol',
+    'Garing',
+    'Receh',
+    'Jayus',
+    'Ajaib',
+    'Unik',
+    'Aneh'
+];
